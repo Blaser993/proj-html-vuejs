22d04e80bb2ca6cda8c1fe64c3005ae3c5a88469
Blaser993 <corradodepinto.design@gmail.com>
2023-03-13 12:46:23 +0100

    START

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+    
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+	export default {
+    
+    components:{
+      Header,
+    }
+
+
+	}
+</script>
+
+<style lang="scss"> //senza scoped in App.vue
+  @use "./style/general.scss"
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    
+        <div class="container">
+            ciao
+        </div>
+    
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="scss" scoped> 
+
+</style>
